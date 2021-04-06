bde20038875e553f62dc62bccc10b067e56191b3
thomasxliu <79129497+thomasxliu@users.noreply.github.com>
2021-04-06 22:57:35 +0800

    import vue resource

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vueresource from 'vue-resource'
+
+Vue.use(vueresource)
 
 Vue.config.productionTip = false
 
