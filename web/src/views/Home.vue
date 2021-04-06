464e1f14967dd6baab0f7d13172329664daca325
thomasxliu <79129497+thomasxliu@users.noreply.github.com>
2021-04-06 22:57:49 +0800

    user authentication with google oauth

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Smart COPD Patient Monitoring System"/>
+    <Login></Login>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Login
+
   }
 }
 </script>
