679c9e85ecb0f50b3541bb8aa50bf22c279eddff
thomasxliu <79129497+thomasxliu@users.noreply.github.com>
2021-04-08 23:02:45 +0800

    add element UI

--- web/src/main.js
+++ web/src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import vueresource from 'vue-resource'
 
+// for element ui, delete in the future if using bootstrap
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
 Vue.use(vueresource)
 
 Vue.config.productionTip = false
