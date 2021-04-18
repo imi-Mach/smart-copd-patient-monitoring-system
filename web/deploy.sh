0889c5e9c96310980a69c02a38690ef8454d8589
Brian Snyder <bts222@lehigh.edu>
2021-04-18 15:00:18 -0400

    added server + deploy files

---
+++ web/deploy.sh
@@ -0,0 +1,29 @@
+……...
+# deploy script for the web front-end
+
+# This file is responsible for preprocessing all TypeScript files, making sure
+# all dependencies are up-to-date, and copying all necessary files into the
+# web deploy directory.
+
+# This is the resource folder where maven expects to find our files
+TARGETFOLDER=../backend/src/main/resources
+
+# This is the folder that we used with the Spark.staticFileLocation command
+WEBFOLDERNAME=web
+
+# step 1: make sure we have someplace to put everything.  We will delete the
+#         old folder tree, and then make it from scratch
+rm -rf $TARGETFOLDER
+mkdir $TARGETFOLDER
+mkdir $TARGETFOLDER/$WEBFOLDERNAME
+
+# step 2: update our npm dependencies
+npm update
+
+# there are many more steps to be done.  For now, we will just copy an HTML file
+cp index.html $TARGETFOLDER/$WEBFOLDERNAME
+
+# step 3: copy jQuery, Handlebars, and Bootstrap files
+cp node_modules/jquery/dist/jquery.min.js $TARGETFOLDER/$WEBFOLDERNAME
+cp node_modules/handlebars/dist/handlebars.min.js $TARGETFOLDER/$WEBFOLDERNAME
+cp node
--- web/src/components/PatientBioData.vue
+++ web/src/components/PatientBioData.vue
@@ -13,6 +13,22 @@
         <el-input v-model="form.rr"></el-input>
       </el-form-item>
 
+      <el-form-item label="Weight: ">
+        <el-input v-model="form.w"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Temperature: ">
+        <el-input v-model="form.t"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Blood Pressure: ">
+        <el-input v-model="form.bp"></el-input>
+      </el-form-item>
+
+      <el-form-item label="Glucose Level: ">
+        <el-input v-model="form.gl"></el-input>
+      </el-form-item>
+
       <el-form-item>
         <el-button type="primary" :plain="true" @click="onSubmit">Submit</el-button>
         <el-button @click="onClear">Clear</el-button>
@@ -22,6 +38,26 @@
 </template>
 
 <script>
+import axios from "axios";
+
+const port = process.env.PORT || '3000'
+
+const apiClient = axios.create({
+  // This can likely just be moved to a non-component page so this only has to be done once
+    baseURL: 'http://localhost:3000',
+    //baseURL = '';
+    //baseURL: 'https://smart-copd-patient.herokuapp.com:' + port,
+    withCredentials: false, // This is the default
+    crossDomain: true,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    timeout: 10000
+})
+
+console.log('PORT:', 'smart-copd-patient.herokuapp.com:' + port) /* eslint-disable-line no-console */
+
 export default {
   name: "PatientBioData",
   data() {
@@ -30,6 +66,10 @@ export default {
         hr: "",
         spo2: "",
         rr: "",
+        w: "",
+        t: "",
+        bp: "",
+        gl: "",
       },
     };
   },
@@ -46,6 +86,8 @@ export default {
       //     '{"hr": this.form.hr, "spo2": this.form.spo2, "rr": this.form.rr}'
       //   );
 
+      this.$http.get
+
       this.$message({
           message: 'Data Successfully Recorded',
           type: 'success'
@@ -59,6 +101,10 @@ export default {
       this.form.hr = "";
       this.form.spo2 = "";
       this.form.rr = "";
+      this.form.w = "";
+      this.form.t = "";
+      this.form.bp = "";
+      this.form.gl = "";
     },
   },
 };
