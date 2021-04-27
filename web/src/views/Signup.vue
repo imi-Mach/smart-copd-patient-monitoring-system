01b35872c7aaf43a591a2a199bf4f81cf041662f
Brian Snyder <bts222@lehigh.edu>
2021-04-27 14:40:50 -0400

    Sign up form

--- web/src/views/Signup.vue
+++ web/src/views/Signup.vue
@@ -1,20 +1,63 @@
 <template>
-    <div>
-        <form id='newReview' onsubmit="return false">
-            <label for='firstName'>First Name</label>
-            <textarea type='text' name='firstName' v-model='firstName' id='firstName' required @keyup="formError = ''"></textarea>
-            <label for='lastName'>Last Name</label>
-            <textarea type='text' name='lastName' v-model='lastName' id='lastName' required @keyup="formError = ''"></textarea>
+  <div class="hero is-fullheight is-info is-bold">
+    <div class="hero-body">
+    <div class="container">
+            <h1 class="title has-text-centered">Vue.js Form Processing</h1>
+            <div class="box">
+                <!-- our signup form ===================== -->
+                <form id="signup-form">
+                    <!-- name -->
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <input 
+                            type="text"
+                            class="input" 
+                            name="name"
+                            v-model="name">
+                    </div>
 
-            <button type="button" id='submitDetails' name='submitDetails' @click="addReview">Post</button>
+                    <!-- email -->
+                    <div class="field">
+                        <label class="label">Email</label>
+                        <input 
+                            type="email" 
+                            class="input" 
+                            name="email" 
+                            v-model="email">
 
-            <h6 id="error">Fill in all fields to submit form.</h6>
-        </form>
+                        <p class="help is-danger" v-if="errors.email">
+                            Please enter a valid email.
+                        </p>
+                    </div>
+
+                    <!-- submit button -->
+                    <div class="field has-text-right">
+                        <button v-on:click="processForm">
+                            Submit            
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
+</div>
 </template>
 
 <script>
-
-    
+    export default {
+        data: () => ({
+            name: '',
+            email: '',
+            errors: {
+                name: false,
+                email: false,
+            }
+        }),
+        methods: {
+            processForm: function() {
+                console.log({ name: this.name, email: this.email });
+                 this.$router.push('home');
+            },
+        }
+    };
 </script>
-
