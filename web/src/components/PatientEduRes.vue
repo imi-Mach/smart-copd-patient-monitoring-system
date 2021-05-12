f4c4a5d80f29e74d11426e78262068dc12ef9ae6
thomasxliu <79129497+thomasxliu@users.noreply.github.com>
2021-05-13 00:15:43 +0800

    adding edu res

--- web/src/components/PatientEduRes.vue
+++ web/src/components/PatientEduRes.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-carousel :interval="4000" type="card" height="400px">
-    <el-carousel-item v-for="item in resources" :key="item">
-      <h3>{{ item }}</h3>
-    </el-carousel-item>
-  </el-carousel>
+  <el-card>
+	 <el-carousel :interval="2000" type="card" height="250px">
+	    <el-carousel-item v-for="item in imgList" :key="item">
+        <a :href="item.link" target="_blank">
+	        <img :src="item.img" style="height:100%;width:100%;">
+          </a>
+	    </el-carousel-item>
+	 </el-carousel>
+ </el-card>
 </template>
 
     <script>
@@ -11,15 +15,28 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      resources: [
-        "resources1",
-        "resources2",
-        "resources3",
-        "resources4",
-        "resources5",
-        "resources6",
+      imgList: [
+        {img: require('@/assets/1.png'), link: "https://www.lvhn.org/find-a-doctor"},
+        {img: require('@/assets/2.png'), link: "https://findadoctor.slhn.org"},
+        {img: require('@/assets/3.png'), link: "https://www.slhn.org/blog/2021/college-students-home-on-break-help-in-fight-against-covid"}
       ],
     };
   },
 };
 </script>
+
+<style>
+.el-carousel__item img {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+</style>
