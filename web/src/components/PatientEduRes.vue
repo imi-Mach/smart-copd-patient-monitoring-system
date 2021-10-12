26126ac399630b890a1380c9a502f3fa9d760cdc
Brian Snyder <bts222@lehigh.edu>
2021-10-12 08:13:55 -0400

    new edu look

--- web/src/components/PatientEduRes.vue
+++ web/src/components/PatientEduRes.vue
@@ -1,16 +1,134 @@
 <template>
-  <el-card>
-	 <el-carousel :interval="2000" type="card" height="250px">
-	    <el-carousel-item v-for="item in imgList" :key="item">
-        <a :href="item.link" target="_blank">
-	        <img :src="item.img" style="height:100%;width:100%;">
-          </a>
-	    </el-carousel-item>
-	 </el-carousel>
- </el-card>
+  <div>
+    <h1>COPD Educational Resources</h1>
+  <el-collapse v-model="activeNames" @change="handleChange" accordion>
+    <el-collapse-item title="Find a Doctor">
+        <el-row :gutter="20">  
+          <el-col :span="10">
+            <div class="grid-content">
+              <el-card class="box-card" shadow="always">
+                <div slot="header" class="header">
+                  <span>Lehigh Valley Health Network</span>
+                  <el-button style="float: right; padding: 3px 0" @click="navContent(1)" type="text">Learn More</el-button>
+                </div>
+                <div class="image">
+                  <img src="../assets/1.png" class="image">
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+          <el-col :span="10">
+            <div class="grid-content">
+              <el-card class="box-card" shadow="always">
+                <div slot="header" class="header">
+                  <span>St. Luke's</span>
+                  <el-button style="float: right; padding: 3px 0" @click="navContent(2)" type="text">Learn More</el-button>
+                </div>
+                <div class="image">
+                  <img src="../assets/2.png" class="image">
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+        </el-row>
+    </el-collapse-item>
+    <el-collapse-item title="COPD Info">
+      <el-row>
+        <el-col :span="10">
+          <div class="grid-content">
+            <el-card class="box-card" shadow="always">
+              <div slot="header" class="header">
+                <span>Is COPD Hereditary?</span>
+                <el-button style="float: right; padding: 3px 0" @click="navContent(3)" type="text">Learn More</el-button>
+              </div>
+              <div class="image2">
+                <img src="../assets/5.jpg" class="image2">
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+        <el-col :span="10">
+          <div class="grid-content">
+            <el-card class="box-card" shadow="always">
+              <div slot="header" class="header">
+                <span>Understanding COPD</span>
+                <el-button style="float: right; padding: 3px 0" @click="navContent(4)" type="text">Learn More</el-button>
+              </div>
+              <div class="image2">
+                <img src="../assets/4.jpg" class="image2">
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+      </el-row>
+    </el-collapse-item>
+    <el-collapse-item title="COPD News">
+        <el-row :gutter="20">  
+          <el-col :span="10">
+            <div class="grid-content">
+              <el-card class="box-card" shadow="always">
+                <div slot="header" class="header">
+                  <span>COVID-19</span>
+                  <el-button style="float: right; padding: 3px 0" @click="navContent(6)" type="text">Learn More</el-button>
+                </div>
+                <div class="image2">
+                  <img src="../assets/3.png" class="image2">
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+          <el-col :span="10">
+            <div class="grid-content">
+              <el-card class="box-card" shadow="always">
+                <div slot="header" class="header">
+                  <span>Lung Cancer Awareness Month</span>
+                  <el-button style="float: right; padding: 3px 0" @click="navContent(7)" type="text">Learn More</el-button>
+                </div>
+                <div class="image2">
+                  <img src="../assets/6.jpg" class="image2">
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+        </el-row>
+    </el-collapse-item>
+    <el-collapse-item title="Community">
+        <el-row :gutter="20">  
+          <el-col :span="10">
+            <div class="grid-content">
+              <el-card class="box-card" shadow="always">
+                <div slot="header" class="header">
+                  <span>Facebook Groups</span>
+                  <el-button style="float: right; padding: 3px 0" @click="navContent(6)" type="text">Learn More</el-button>
+                </div>
+                <div class="image2">
+                  <img src="../assets/7.png" class="image">
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+          <el-col :span="10">
+            <div class="grid-content">
+              <el-card class="box-card" shadow="always">
+                <div slot="header" class="header">
+                  <span>Finding a Support Group</span>
+                  <el-button style="float: right; padding: 3px 0" @click="navContent(7)" type="text">Learn More</el-button>
+                </div>
+                <div class="image2">
+                  <img src="../assets/8.jpg" class="image2">
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+        </el-row>
+    </el-collapse-item>
+  </el-collapse>
+  </div>
 </template>
 
-    <script>
+<script>
+
+
 export default {
   name: "HelloWorld",
   data() {
@@ -22,21 +140,74 @@ export default {
       ],
     };
   },
+  methods: {
+    navContent(index) {
+      switch (index) {
+        case 1:
+          window.location.href = "https://www.lvhn.org/find-a-doctor";
+          break;
+        case 2:
+          window.location.href = "https://findadoctor.slhn.org/";
+          break;
+        case 3:
+          window.location.href = "https://www.slhn.org/blog/2021/college-students-home-on-break-help-in-fight-against-covid";
+          break;
+        case 4:
+          window.location.href = "https://easyoxygen.com.au/blogs/articles/understanding-copd-medical-oxygen-help";
+          break;
+        case 5:
+          window.location.href = "https://www.healthline.com/health/copd/is-copd-hereditary";
+          break;
+        case 6:
+          window.location.href = "https://www.unitypoint.org/article.aspx?id=2dbeb838-11b2-47d5-9cc7-95c8a9f28631";
+          break;
+        case 7:
+          window.location.href = "https://www.facebook.com/COPD-Friends-for-Friends-110451268982117/";
+          break;
+        case 8:
+          window.location.href = "https://www.lung.org/lung-health-diseases/lung-disease-lookup/copd/living-with-copd/finding-support";
+          break;
+      }
+    }
+  }
 };
 </script>
 
 <style>
-.el-carousel__item img {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 200px;
-  margin: 0;
-}
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-.el-carousel__item:nth-child(2n+1) {
-  background-color: #d3dce6;
-}
+
+  .title {
+    float: "left";
+    font-size: 42px;
+  }
+
+  .header {
+    font-size: 24px;
+    font-weight: bold;
+    float: "center";
+  }
+
+  .image {
+     height: 100%;
+     padding-top: 10px;
+  }
+
+  .image2 {
+    height: 140px;
+    padding-bottom: 10px;
+  }
+
+  .box-card {
+    width: 40em;
+    height: 250px;
+    float: "center";
+    text-align: "center";
+  }
+
+  .el-col {
+    border-radius: 4px;
+  } 
+  .grid-content {
+    border-radius: 4px;
+    min-height: 400px;
+  }
 </style>
