82e86c77c1911cf95c16719f44c3140b0ff35178
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2021-11-22 20:35:25 -0500

    button color change

--- web/src/views/Register.vue
+++ web/src/views/Register.vue
@@ -34,8 +34,12 @@
         <el-form-item label="Phone Number: ">
           <el-input v-model="phoneNumber" class="uinput"></el-input>
         </el-form-item>
-
+        <template v-if="isPatient">
+        <el-button type="primary" @click="processForm">Submit</el-button>
+        </template>
+        <template v-if="isHealthCare">
         <el-button type="warning" @click="processForm">Submit</el-button>
+        </template>
         <!-- TODO: Cancel will need to sign out the user -->
         <el-button>Cancel</el-button>
       </el-form>
